ab309c5e9ab6d49d8feccb38c5dec9e018e9bfaf
Jaweed Khan <mr.jaweedkhan@gmail.com>
2023-05-16 22:25:47 +0530

    updated add user and update user api

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'mydatabase'
+    database: 'tickets_system'
 });
 
 db.connect((err) => {
--- server/routes/users.js
+++ server/routes/users.js
@@ -7,32 +7,33 @@ const { body, validationResult } = require('express-validator');
 router.post('/', [
     body('name').not().isEmpty().trim().escape(),
     body('email').isEmail().normalizeEmail(),
-    body('password').isLength({ min:8 }).withMessage('must be 8 chars long'),
+    body('password').isLength({ min: 8 }).withMessage('must be 8 chars long'),
 ], (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array()})
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
     }
     const { name, email, password } = req.body;
+    const status = 1;
+    const created_at = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    const updated_at = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
-    /**
-     * check if user exists with email
-     */
+    // Check if user exists with email
     db.query('SELECT * FROM users WHERE email = ?', email, (err, result) => {
-        if(err){
-            res.status(500).json({ message: err.message })
+        if (err) {
+            return res.status(500).json({ message: err.message });
         }
-        if(result.length > 0){
-            res.status(200).json({ message: 'User already exists'})
+        if (result.length > 0) {
+            return res.status(200).json({ message: 'User already exists' });
         }
-    });
 
-
-    db.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password], (err, result) => {
-        if(err){
-            res.status(500).json({ message: err.message })
-        }
-        res.status(201).json({ message: 'User created successfully'});
+        // User does not exist, insert into the database
+        db.query('INSERT INTO users (name, email, password, status, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)', [name, email, password, status, created_at, updated_at], (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: err.message });
+            }
+            return res.status(201).json({ message: 'User created successfully' });
+        });
     });
 });
 
@@ -42,16 +43,28 @@ router.put('/:id', [
     body('email').isEmail().normalizeEmail(),
 ], (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array()})
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
     }
     const id = req.params.id;
     const { name, email } = req.body;
-    db.query('UPDATE users SET name = ?, email = ?, WHERE id = ?', [name, email, id], (err, result) => {
-        if(err){
-            res.status(500).json({ message: err.message })
+
+    // Check if the new email is already associated with another user
+    db.query('SELECT * FROM users WHERE email = ? AND id <> ?', [email, id], (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: err.message });
         }
-        res.status(200).json({ message: 'User updated successfully'});
+        if (result.length > 0) {
+            return res.status(400).json({ message: 'Email is already in use' });
+        }
+
+        // Update the user's name and email
+        db.query('UPDATE users SET name = ?, email = ? WHERE id = ?', [name, email, id], (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: err.message });
+            }
+            return res.status(200).json({ message: 'User updated successfully' });
+        });
     });
 });
 
@@ -66,7 +79,7 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-// List usrers API
+// List users API
 router.get('/', (req, res) => {
    db.query('SELECT * FROM users', (err, result) => {
       if(err){
